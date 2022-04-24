4457d2f23cd22dc9538c2277f03fd1dd2996344b
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-04-24 13:17:43 +0530

    Fetch data & imgscompleted

--- API.py
+++ API.py
@@ -1,6 +1,7 @@
 import tweepy
 from tweepy.errors import Unauthorized
 import os
+import wget
 
 BEARER_TOKEN=os.environ.get('BEARER_TOKEN') or ""
 API_KEY=os.environ.get('API_KEY') or ""
@@ -9,13 +10,56 @@ ACCESS_TOKEN=os.environ.get('ACCESS_TOKEN') or ""
 ACCESS_TOKEN_SECRET=os.environ.get('ACCESS_TOKEN_SECRET') or "" 
 
 def fetch_data():
-    ''' Returns a dictionary with keys as usernames and values as list of image_path saved in a folder tmp_img/low_res'''
+    ''' Returns an array with keys as usernames and values as list of image_path saved in a folder tmp_img/low_res'''
+    tweets = []
+    
     try:
         client = tweepy.Client(bearer_token=BEARER_TOKEN, consumer_key=API_KEY, 
                             consumer_secret=API_KEY_SECRET, access_token=ACCESS_TOKEN, 
                             access_token_secret=ACCESS_TOKEN_SECRET)
         schwyte = client.get_me()
         user_id = schwyte.data.id
+        public_tweets = client.get_users_mentions(user_id,expansions='attachments.media_keys',media_fields=["url","preview_image_url"])
+
+        for response in tweepy.Paginator(client.get_users_mentions,user_id,expansions=['attachments.media_keys','author_id'],media_fields=["url","height","width","preview_image_url"]):
+            print(response)
+            try:
+                media = {m["media_key"]: m for m in response.includes['media']}
+                print(media)
+            except:
+                media = False
+
+            client_user_ids = [tweet.author_id for tweet in response.data]
+            # # print(client_user_ids)
+
+            client_users = client.get_users(ids = client_user_ids)
+            client_user_names = {user.id:user.username for user in client_users.data}
+            # # print(client_user_names)
+            
+            for tweet in response.data:
+                print('tweet:',tweet.text,"\n author_id:",tweet.author_id,"\n tweet_id: ",tweet.id,"\n")
+                current_tweet = {
+                    "tweet":tweet.text,
+                    "username": client_user_names[tweet.author_id],
+                    "author_id":tweet.author_id,
+                    "tweet_id":tweet.id
+                }
+                if tweet.attachments:
+                    attachments = tweet['attachments']
+                    media_keys = attachments['media_keys']
+                    current_tweet['media_keys'] = media_keys
+                    current_tweet["low_res_imgs"] = []
+                    
+                    for key in media_keys: 
+                        if media[key].type == "photo":
+                            file_name = media[key].url.replace("https://pbs.twimg.com/media/","")
+                            file_loc = "tmp/low_res/" + file_name
+                            current_tweet["low_res_img"].append(file_name)
+                            if not os.path.exists(file_loc):
+                                wget.download(media[key].url,file_loc)
+                            tweets.append(current_tweet)
+
+
         
     except Unauthorized:
         print("Unauthorized! Check API keys")
@@ -23,8 +67,8 @@ def fetch_data():
     except Exception as e:
         print(f"Error occured: {e}")
         quit()
-        
-    return {}
+    print(tweets)
+    return tweets
 
 def post_imgs(img_paths, username):
     '''Posts a list of images with a msg mentioning twitter username'''
