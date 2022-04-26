f23ea2660ca1a23afad97ef0410d8a7a3cd17b0a
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-04-26 11:54:15 +0530

    Folders

--- release.sh
+++ release.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+DIRECTORY='tmp'
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir tmp && mkdir tmp/hi_res && mkdir tmp/low_res
+  # Control will enter here if $DIRECTORY doesn't exist.
+fi
 pip install 'h5py==2.10.0' --force-reinstall
 pip install git+https://github.com/idealo/image-super-resolution
