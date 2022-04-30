2c86512092549c51fe3164ba9361ad59802ce93b
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-05-01 00:19:36 +0530

    D

--- ISR_resolve.py
+++ ISR_resolve.py
@@ -3,12 +3,12 @@ import numpy as np
 from PIL import Image
 from ISR.models import RDN
 
-rdn = RDN(weights='noise-cancel')
 
 def super_resolve(img_paths):
     '''converts low res to hi res images
     args: image paths in tmp_img
     returns: status if converted correctly and list of paths of super resolved images'''
+    rdn = RDN(weights='noise-cancel')
     low_res_dir = "tmp/low_res/"
     hi_res_dir = "tmp/hi_res/"
     for path in img_paths:
