7d19d22f2a278b92cc23bed9271d57388d696360
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-05-05 16:21:10 +0530

    &

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ DIRECTORY='tmp'
 if [ ! -d "$DIRECTORY" ]; then
     mkdir tmp && mkdir tmp/hi_res && mkdir tmp/low_res
 fi
+pip install ISR
 pip install 'h5py==2.10.0' --force-reinstall
 # pip install git+https://github.com/idealo/image-super-resolution
-pip install ISR
 pip install -r requirements.txt
